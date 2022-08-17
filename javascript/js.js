c30c5df5e862bcc1e97021914d71181dba92901c
Marko Maturino <76288157+hybridmarko720@users.noreply.github.com>
2022-08-17 00:33:58 -0500

    Add addNum() Function
    Also working on conCatNumbers() function as well

--- javascript/js.js
+++ javascript/js.js
@@ -3,129 +3,73 @@ const results = document.getElementById("Results")
 let firstNum = ""
 let secondNum = ""
 let result = ""
+function addNum(x){
+    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("/") || firstNum.includes("*")){
+    secondNum += x
+    results.innerText = firstNum + secondNum
+    return secondNum
+}
+else{firstNum += x
+results.innerText = firstNum 
+return firstNum}
 
+}
+function conCatNumbers(x){
+    results.innerText = firstNum
+    firstNum = firstNum + x 
+    return firstNum
+
+}
 // Global Variables End
 
 // Numbers Section Start
 document.getElementById("zero").addEventListener("click", addZero)
 function addZero(){
-    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("*")|| firstNum.includes("/")){
-        secondNum += 0
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 0
-    results.innerText = firstNum 
-    return firstNum}
+   return addNum(0)
     
 }
 document.getElementById("one").addEventListener("click", addOne)
 function addOne(){
-    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("/")){
-        secondNum += 1
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 1
-    results.innerText = firstNum 
-    return firstNum}
+    return addNum(1)
 }
 document.getElementById("two").addEventListener("click", addTwo)
 function addTwo(){
-    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("*")|| firstNum.includes("/")){
-        secondNum += 2
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 2
-    results.innerText = firstNum 
-    return firstNum}
+    return addNum(2)
 }
 document.getElementById("three").addEventListener("click", addThree)
 function addThree(){
-    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("*")|| firstNum.includes("/")){
-        secondNum += 3
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 3
-    results.innerText = firstNum 
-    return firstNum}
+    return addNum(3)
 }
 document.getElementById("four").addEventListener("click", addFour)
 function addFour(){
-    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("*")|| firstNum.includes("/")){
-        secondNum += 4
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 4
-    results.innerText = firstNum 
-    return firstNum}
+    return addNum(4)
 }
 document.getElementById("five").addEventListener("click", addFive)
 function addFive(){
-    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("*")|| firstNum.includes("/")){
-        secondNum += 5
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 5
-    results.innerText = firstNum 
-    return firstNum}
+    return addNum(5)
 }
 document.getElementById("six").addEventListener("click", addSix)
 function addSix(){
-    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("*")|| firstNum.includes("/")){
-        secondNum += 6
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 6
-    results.innerText = firstNum 
-    return firstNum}
+    return addNum(6)
 }
 document.getElementById("seven").addEventListener("click", addSeven)
 function addSeven(){
-    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("*")|| firstNum.includes("/")){
-        secondNum += 7
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 7
-    results.innerText = firstNum 
-    return firstNum}
+    return addNum(7)
 }
 document.getElementById("eight").addEventListener("click", addEight)
 function addEight(){
-    if(firstNum.includes("+")|| firstNum.includes("-")|| firstNum.includes("*")|| firstNum.includes("/")){
-        secondNum += 8
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 8
-    results.innerText = firstNum 
-    return firstNum}
+    return addNum(8)
 }
 document.getElementById("nine").addEventListener("click", addNine)
 function addNine(){
-    if(firstNum.includes("+") || firstNum.includes("-")|| firstNum.includes("*")|| firstNum.includes("/")){
-        secondNum += 9
-        results.innerText = firstNum + secondNum
-        return secondNum
-    }
-    else{firstNum += 9
-    results.innerText = firstNum 
-    return firstNum}
+    return addNum(9)
 }
 // Number Section End
 
 // Operataion Section Start
 document.getElementById("add").addEventListener("click", add)
 function add(){
-    results.innerText = firstNum
-    firstNum = firstNum + "+" 
-    return firstNum
+    return conCatNumbers("+")
 } 
 document.getElementById("minus").addEventListener("click", minus)
 function minus(){
